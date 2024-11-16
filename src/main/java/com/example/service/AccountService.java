849d17fd3d7ef6315ff121244c231e6c333c10b3
Syed Ahtsham Ul Hassan <syedahtshamqau@gmail.com>
2024-11-16 01:19:30 +0000

    Added more comments and final commit

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -18,13 +18,15 @@ public class AccountService {
         this.accountRepository = accountRepository;
     }
 
+    // This method will create a new account after checking if there is no account exists with this given username
     public Account createAccount(Account account){
         if(accountRepository.findAccountByUsername(account.getUsername())!=null){
             return null;
         }
         return accountRepository.save(account);
     }
-
+    
+    // this method will log in a user by verifying its credentials
     public Account loginAccount(Account account){
         return accountRepository.findAccountByUsernameAndPassword(account.getUsername(),account.getPassword());
     }
